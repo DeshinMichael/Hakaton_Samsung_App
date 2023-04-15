dc2e7bfb16a49c100b7375f7748c91a868ce6d6a
DeshinMichael <deshinmisha26@gmail.com>
2023-04-15 20:27:02 +0800

    clean

--- HackathonHTTPClientError400/app/src/main/java/com/example/hackathon_httpclienterror_400/feature/main/ui/MainActivity.java
+++ HackathonHTTPClientError400/app/src/main/java/com/example/hackathon_httpclienterror_400/feature/main/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.hackathon_httpclienterror_400;
+package com.example.hackathon_httpclienterror_400.feature.main.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.hackathon_httpclienterror_400.R;
 
 public class MainActivity extends AppCompatActivity {
 
